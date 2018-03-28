7e738293d88fe861a9039fcda636f3fe5aff3af5
zhongjibing <zhongjibing@163.com>
2018-03-28 21:35:55 +0800

    add

---
+++ service-feign/src/main/java/com/icezhg/springcloud/servicefeign/controller/HiController.java
@@ -0,0 +1,20 @@
+package com.icezhg.springcloud.servicefeign.controller;
+
+import com.icezhg.springcloud.servicefeign.api.SchedualServiceHi;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HiController {
+
+    @Autowired
+    private SchedualServiceHi schedualServiceHi;
+
+    @RequestMapping(value = "/hi", method = RequestMethod.GET)
+    public String sayHi(@RequestParam String name) {
+        return schedualServiceHi.sayHiFromClientOne(name);
+    }
+}
